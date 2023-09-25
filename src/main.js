ff17d81060de18d2c434f1b5dd34c7eae0d00c03
Jodie Kurnia <jodiekurnia@outlook.com>
2023-09-25 18:44:58 +0700

    bener?

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-(function($){"use strict";})(jQuery);
+// '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
+$(window).on("load resize ", function() {
+    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+    $('.tbl-header').css({'padding-right':scrollWidth});
+  }).resize();
